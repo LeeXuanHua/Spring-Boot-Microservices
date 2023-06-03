35861f75d6eeba98b8236d738aa623acb68a0e13
Lee Xuan Hua <46639916+LeeXuanHua@users.noreply.github.com>
2023-06-03 19:56:29 +0800

    Implemented inventory service

---
+++ inventory-service/src/main/java/com/microservices/inventoryservice/model/Inventory.java
@@ -0,0 +1,21 @@
+package com.microservices.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "t_inventory")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String skuCode;
+    private int quantity;
+}
